cd85d512bf8cc94da92b90e7b6b18cbd85566b4a
IvanNS <ivan_ns3@abv.bg>
2021-04-05 10:09:16 +0200

    First commit

---
+++ components/Canvas.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Canvas = props => {
+    return ""
+}
+
+const styles = StyleSheet.create({
+    screen:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default Canvas;
---
+++ components/MenuBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const MenuBar = props => {
+    return ""
+}
+
+const styles = StyleSheet.create({
+    screen:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default MenuBar;
