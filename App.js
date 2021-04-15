53b16ecb4766dddb13a3d4646d375356ff7abf5b
IvanNS <ivan_ns3@abv.bg>
2021-04-15 10:55:48 +0200

    Changes to the orientation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { setStatusBarBackgroundColor } from 'expo-status-bar';
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Dimensions, PanResponder, View, StyleSheet, Button } from 'react-native';
 import Svg, { Polyline } from 'react-native-svg';
 
@@ -15,7 +15,7 @@ const GesturePath = ({ path, color, strokeWidth }) => {
   const { width, height } = Dimensions.get('window');
   const points = path.map(p => `${p.x},${p.y}`).join(' ');
   return (
-    <Svg height="90%" width="100%" viewBox={`0 0 ${width} ${height}`}>
+    <Svg height="100%" width="100%" viewBox={`0 0 ${width} ${height}`}>
       <Polyline
         points={points}
         fill="none"
@@ -52,18 +52,46 @@ const GestureRecorder = ({ onPathChanged }) => {
   );
 }
 
+
 const App = () => {
   const [path, setPath] = useState(examplePath);
   const [color, setColor] = useState('green');
   const [strokeWidth, setStrokeWidth] = useState('1');
+  const [availableDeviceWidth, setAvailableDeviceWidth] = useState(
+    Dimensions.get('window').width
+  );
+  const [availableDeviceHeight, setAvailableDeviceHeight] = useState(
+    Dimensions.get('window').height
+  );
+  useEffect(() => {
+    const updateLayout = () => {
+      setAvailableDeviceHeight(Dimensions.get('window').height);
+      setAvailableDeviceWidth(Dimensions.get('window').width);
+    };
+    Dimensions.addEventListener('change', updateLayout)
+
+    return () => {Dimensions.removeEventListener('change',updateLayout); };
+  });
+  if (availableDeviceWidth < availableDeviceHeight) {
   return (
     <View style={StyleSheet.absoluteFill}>
       <GesturePath path={path} color={color} strokeWidth={strokeWidth} />
       <GestureRecorder onPathChanged={setPath} />
-      <Button title='red' onPress={() => setColor("red")}></Button>
+      <View style={{zIndex: 0,position:'absolute',top:'90%',flexDirection:'row'}}><Button title='red' onPress={() => setColor("red")}></Button>
       <Button title='1.5' onPress={() => setStrokeWidth("1.5")}></Button>
+      </View>
     </View>
   );
+}
+return (
+  <View style={StyleSheet.absoluteFill}>
+    <GesturePath path={path} color={color} strokeWidth={strokeWidth} />
+    <GestureRecorder onPathChanged={setPath} />
+    <View style={{zIndex: 0,position:'absolute',top:'10%',flexDirection:'row'}}><Button title='red' onPress={() => setColor("red")}></Button>
+    <Button title='1.5' onPress={() => setStrokeWidth("1.5")}></Button>
+    </View>
+  </View>
+);
 }
 
 export default App
